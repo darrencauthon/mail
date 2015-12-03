9b68fd6e759f6b219e65c4fcd25b32d1f5bb7cfb
Darren Cauthon <darren@cauthon.com>
2015-12-02 21:45:48 -0600

    More passing tests.

--- spec/mail/core_extensions_spec.rb
+++ spec/mail/core_extensions_spec.rb
@@ -5,45 +5,45 @@ describe Object do
 
   describe "blank method" do
     it "should say nil is blank" do
-      expect(nil).to be_blank
+      expect(Mail::StringHelper.blank?(nil)).to be_truthy
     end
 
     it "should say false is blank" do
-      expect(false).to be_blank
+      expect(Mail::StringHelper.blank?(false)).to be_truthy
     end
 
     it "should say true is not blank" do
-      expect(true).not_to be_blank
+      expect(Mail::StringHelper.blank?(true)).not_to be_truthy
     end
 
     it "should say an empty array is blank" do
-      expect([]).to be_blank
+      expect(Mail::StringHelper.blank?([])).to be_truthy
     end
 
     it "should say an empty hash is blank" do
-      expect({}).to be_blank
+      expect(Mail::StringHelper.blank?({})).to be_truthy
     end
 
     it "should say an empty string is blank" do
-      expect('').to be_blank
-      expect(" ").to be_blank
+      expect(Mail::StringHelper.blank?('')).to be_truthy
+      expect(Mail::StringHelper.blank?(" ")).to be_truthy
       a = ''; 1000.times { a << ' ' }
-      expect(a).to be_blank
-      expect("\t \n\n").to be_blank
+      expect(Mail::StringHelper.blank?(a)).to be_truthy
+      expect(Mail::StringHelper.blank?("\t \n\n")).to be_truthy
     end
   end
 
   describe "not blank method" do
     it "should say a number is not blank" do
-      expect(1).not_to be_blank
+      expect(Mail::StringHelper.blank?(1)).not_to be_truthy
     end
     
     it "should say a valueless hash is not blank" do
-      expect({:one => nil, :two => nil}).not_to be_blank
+      expect(Mail::StringHelper.blank?({:one => nil, :two => nil})).not_to be_truthy
     end
     
     it "should say a hash containing an empty hash is not blank" do
-      expect({:key => {}}).not_to be_blank
+      expect(Mail::StringHelper.blank?({:key => {}})).not_to be_truthy
     end
   end
 
